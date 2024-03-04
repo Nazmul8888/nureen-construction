023d1abd7e53f0cead9914ba8f49b103d7f03af2
Nazmul8888 <nazmulkhan416@gmail.com>
2024-03-04 17:55:43 +0600

    first commit

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Banner from "../Sharedpage/Banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Sharedpage/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+
+
+const Banner = () => {
+    
+    return (
+        <div>
+            <img src="https://i.ibb.co/k6CGY6q/nes6-spiz-211203.jpg" alt="" />
+        </div>
+    );
+};
+
+export default Banner;
